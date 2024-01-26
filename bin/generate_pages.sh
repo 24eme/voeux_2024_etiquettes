fb81024bc7ae0575cf9d8104f0d34a173d6e81c0
Vincent LAURENT <vince.laurent@gmail.com>
2024-01-26 15:50:52 +0100

    index en fiche

--- bin/generate_pages.sh
+++ bin/generate_pages.sh
@@ -9,5 +9,5 @@ do
   bash bin/generate_etiquette.sh "$csvline"
   ETIQUETTESVG_BASE64=$(cat generate/etiquettes/"$QRCODE"_recto.svg | base64 -w 0)
   BOUTEILLESVG_BASE64=$(echo "s|%RECTO_BASE64%|$ETIQUETTESVG_BASE64|" | sed -f - templates/bouteille_brute_rouge_etiquette.svg | base64 -w 0)
-  echo "s|bouteille_brute_rouge.svg|data:image/svg+xml;base64,$BOUTEILLESVG_BASE64|" | sed -f - templates/index.html | sed "s/%CUVEE%/$CUVEE/" > generate/pages/$QRCODE.html
+  echo "s|bouteille_brute_rouge.svg|data:image/svg+xml;base64,$BOUTEILLESVG_BASE64|" | sed -f - templates/fiche.html | sed "s/%CUVEE%/$CUVEE/" > generate/pages/$QRCODE.html
 done
